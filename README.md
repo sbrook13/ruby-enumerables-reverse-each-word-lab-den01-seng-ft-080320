77a4502b73eaca8697031df581884766f1a905d6
Shelley Brook <sbrook13@gmail.com>
2020-07-19 17:53:26 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Reverse Each Word
+se_# Reverse Each Word
 
 ## Learning Goals
 
---
+++ reverse_each_word.rb
@@ -0,0 +1,33 @@
+require 'pry'
+def reverse_each_word(str)
+  words = str.split(" ")
+  new_word = []
+  words.collect {|w| 
+    new_word = w.split("")
+    new_word = new_word.reverse!
+    new_word = new_word.join("")
+  }
+  new_word
+end
+binding.pry
+
+#def reverse_each_word(str)
+#  words = str.split(" ")
+#  i = 0 
+#  reversed_words = []
+#  while i < words.length do 
+#    new_word = words[i].split("")
+#    new_word = new_word.reverse
+#    new_word = new_word.join("")
+#    reversed_words << new_word
+#   i+= 1 
+#  end 
+#  reversed_words.join(" ")
+#end
+
+
+# split sentence into words
+# split words into letters
+# reverse order of letters
+# join letters into words
+# join words in sentence
